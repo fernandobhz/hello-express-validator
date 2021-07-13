d3978a0724d1821341ea57d9c9833f770b3eb597
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-07-13 13:28:31 -0300

    chore: express-validator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', body: "" });
+});
+
+let validations = [
+  check('nome').notEmpty().withMessage('Nome é requerido'),
+  check('idade').isInt().withMessage('Idade é requerido e deve ser um inteiro'),
+];
+
+router.post('/', validations, function(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessage = errors.errors.map(item => `${item.param}: ${item.msg}`).join("\n");
+    throw new Error(errorMessage);
+  }
+
+  res.render('index', { title: 'Express', body: req.body });
 });
 
 module.exports = router;
